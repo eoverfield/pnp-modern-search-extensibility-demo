ce2302da06fa658fcb5ffdc9ef582431af01fe37
Eric Overfield <eoverfield@pixelmill.com>
2023-05-04 10:44:10 -0700

    Conference clean up

--- pnp-modern-search-main/search-parts/src/controls/PropertyPaneTabsField/components/IChoiceGroupTabsProps.ts
+++
@@ -1,7 +0,0 @@
-import { IChoiceGroupOption } from "office-ui-fabric-react";
-
-export interface IChoiceGroupTabsProps {
-    defaultSelectedKey: string;
-    onChange(selectedKey: string): void;
-    options: IChoiceGroupOption[];
-}
